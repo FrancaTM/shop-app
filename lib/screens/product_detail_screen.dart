a67b8ddd9fe8a56e45a50198ae1eae1de6dc5cbe
FrancaTM <francatm@outlook.com>
2019-08-11 23:22:44 -0300

    load product into detail screen

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'package:my_shop_app/providers/products_provider.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   // final String title;
@@ -9,9 +12,12 @@ class ProductDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final productId = ModalRoute.of(context).settings.arguments as String; // id
+    final loadedProduct = Provider.of<ProductsProvider>(context, listen: false)
+        .findById(productId);
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('title'),
+        title: Text(loadedProduct.title),
       ),
     );
   }
