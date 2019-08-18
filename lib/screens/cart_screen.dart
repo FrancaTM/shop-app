bddead5e8eec9b7017076630eab44b5455b190e9
FrancaTM <francatm@outlook.com>
2019-08-18 00:31:49 -0300

    add cart item widget

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import 'package:my_shop_app/providers/cart_provider.dart';
+import 'package:my_shop_app/providers/cart_provider.dart' show CartProvider;
+import 'package:my_shop_app/widgets/cart_item.dart';
 
 class CartScreen extends StatelessWidget {
   static const routeName = '/cart';
@@ -43,6 +44,18 @@ class CartScreen extends StatelessWidget {
                     onPressed: () {},
                   ),
                 ],
+              ),
+            ),
+          ),
+          SizedBox(height: 10),
+          Expanded(
+            child: ListView.builder(
+              itemCount: cart.items.length,
+              itemBuilder: (ctx, index) => CartItem(
+                id: cart.items.values.toList()[index].id,
+                price: cart.items.values.toList()[index].price,
+                quantity: cart.items.values.toList()[index].quantity,
+                title: cart.items.values.toList()[index].title,
               ),
             ),
           ),
