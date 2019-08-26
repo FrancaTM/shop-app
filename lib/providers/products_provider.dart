a714a33fe7c6f45790beb953fcd57c54f02ab5a6
FrancaTM <francatm@outlook.com>
2019-08-26 16:26:50 -0300

    add optimistic updating and custom exception

--- lib/providers/products_provider.dart
+++ lib/providers/products_provider.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
+import 'package:my_shop_app/models/http_exception.dart';
 import 'package:my_shop_app/providers/product.dart';
 
 class ProductsProvider with ChangeNotifier {
@@ -151,8 +152,25 @@ class ProductsProvider with ChangeNotifier {
     }
   }
 
-  void deleteProduct(String id) {
-    _items.removeWhere((prod) => prod.id == id);
+  Future<void> deleteProduct(String id) async {
+    final url =
+        'https://flutter-shop-app-3cfa0.firebaseio.com/products/$id.json';
+
+    // optimistic updating
+    final existingProductIndex = _items.indexWhere((prod) => prod.id == id);
+    var existingProduct = _items[existingProductIndex];
+
+    _items.removeAt(existingProductIndex);
     notifyListeners();
+
+    final response = await http.delete(url);
+    if (response.statusCode >= 400) {
+      // revert updating
+      _items.insert(existingProductIndex, existingProduct);
+      notifyListeners();
+
+      throw HttpException('Could not delete product');
+    }
+    existingProduct = null;
   }
 }
